165aea2a399a041b461e36818ab0c10d849ca68f
Mariia Osipova <maryosipovaa@gmail.com>
2025-10-10 18:18:27 -0300

    feat: add main entry point calling mosaico and voronoi modules

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
+from filters.mosaico import mosaico
+from filters.voronoi import voronoi
+
+
 def main():
     path = input("Ingrese la ruta de la imagen: ")
     return path
+
+mosaico(path=main())
+voronoi(path=main())
 
 if __name__ == "__main__":
     main()
